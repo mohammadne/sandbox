76cb093c5839f405cb4d57ab4e666b9323e75882
mohammadne <mohammadne.dev@gmail.com>
2022-11-30 11:49:21 +0330

    feat: add pointers

---
+++ learn-rust/src/pointer.rs
@@ -0,0 +1,11 @@
+pub fn run() {
+    // primitives
+    let arr1 = [1, 2, 3];
+    let arr2 = arr1;
+    println!("{:?}", (arr1, arr2));
+
+    // non-primitives
+    let vec1 = vec![1, 2, 3];
+    let vec2 = &vec1;
+    println!("{:?}", (vec1, vec2));
+}
